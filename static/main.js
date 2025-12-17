8c2cd54bc55438255f9632935fccc60ab1729249
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2025-12-17 16:02:00 -0500

    Add some JS

---
+++ static/main.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("giftForm");
+const giftsContainer = document.getElementById("gifts");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = form.elements.name.value;
+	const gift = form.elements.gift.value;
+
+	const item = document.createElement("p");
+	item.textContent = `Gift for ${name}: ${gift}`;
+	giftsContainer.appendChild(item);
+
+	form.reset();
+});
+
